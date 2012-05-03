1f17d9851d5d753a7cf1b834f86a85681f405573
rtsinani <artisinani@gmail.com>
2012-05-03 22:56:30 +0100

    add comments

--- colonne.view.js
+++ colonne.view.js
@@ -2,13 +2,19 @@
 	var Colonne = window.Colonne = {};
 	
 	Colonne.View = Backbone.View.extend({
+		// Points to the underscore library by default
+		// Override it with the library of choice
 		template	: _.template,
+		
+		// Conventionally a result of appending '-template' to element id
 		templateId: '',
 		
+		// Calls renderTemplated by default, but can be overriden to fit the needs of your app
 		render: function () {
 			return this.renderTemplated();
 		},
 		
+		// Renders the template provided by default
 		renderTemplated: function () {
 			if (this.model && this._setTemplateId()) {
 					var tmpl = this._getTemplate();
@@ -17,11 +23,14 @@
 			return this;
 		},
 		
+		// Private
+
 		_setTemplateId: function () {
 			if (!this.el || !this.el.id) return false;
 			this.templateId = [this.el.id, 'template'].join('-');
 			return true;
 		},
+		
 		
 		_getTemplate: function () {
 			if (Colonne.View.TemplateStore[this.templateId]) return Colonne.View.TemplateStore[this.templateId];
